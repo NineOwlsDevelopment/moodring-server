09c0703d2405f69af2a82ffa961635cf8ecffd3d
nineowls <nineowls@Mac-mini.local>
2025-12-24 03:11:47 -0600

    add port

--- src/scripts/migrate.sh
+++ src/scripts/migrate.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+set -e
+
 source ../.env
+
 for migration in ../migrations/*.sql; do
   echo "Running migration: $migration"
-  PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f "$migration"
+  PGPASSWORD="$DB_PASSWORD" psql \
+    -h "$DB_HOST" \
+    -p "$DB_PORT" \
+    -U "$DB_USER" \
+    -d "$DB_SSL" \
+    --set=sslmode="$DB_SSLMODE" \
+    -f "$migration"
 done
