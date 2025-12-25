138965bae92d25104f038d353b4be14826bc67e7
nineowls <nineowls@Mac-mini.local>
2025-12-24 20:37:16 -0600

    add updates

--- client/dist/assets/arrow-left-CdksnB87.js
+++ client/dist/assets/arrow-left-CdksnB87.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-BZmdOFNw.js";/**
+import{c as o}from"./index-ro5SsOap.js";/**
  * @license lucide-react v0.556.0 - ISC
  *
  * This source code is licensed under the ISC license.
